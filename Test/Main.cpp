f5586b05858f67f4559bb762e766c5304b7df0da
Ackerfe <acker.federico@gmail.com>
2020-01-17 18:01:27 +0100

    Added basic colour rendering test
    Tried some basic openGL colour rendering (added some code to WindowHandler.cpp in the Ackerfe Package and main.cpp in the Test Package)

--- Test/Main.cpp
+++ Test/Main.cpp
@@ -1,6 +1,7 @@
 #include <InputHandler.h>
 #include <WindowHandler.h>
 #include <SDL/SDL.h>
+
 /*temporary*/
 #include <iostream>
 
@@ -8,7 +9,9 @@ int main(int argc, char** argv)
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-
+	float red = 0.0f;
+	float green = 0.0f;
+	float blue = 0.0f;
 	Ackerfe::WindowHandler newWindow;
 	newWindow.createWindow("Window", 500, 500, 0);
 
@@ -19,13 +22,31 @@ int main(int argc, char** argv)
 
 	while (!doQuit) 
 	{
+		glClearColor(red, green, blue, 1.0f);
+		
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		newInput.inputQueue();
 		if (newInput.isKeyDown(SDLK_w))
 		{
 			doQuit = true;
 		}
-		else
+		else {
 			newWindow.swapBuffer();
-	}
+		}
+		if (red >= 1.0f)
+			red = 0.0f;
+		else
+			red += 0.0001f;
+		if (green >= 1.0f)
+			green = 0.0f;
+		else
+			green += 0.0002f;
+		if (blue >= 1.0f)
+			blue = 0.0f;
+		else
+			blue += 0.0003f;
+
+		
+	};
 	return 0;
 }
